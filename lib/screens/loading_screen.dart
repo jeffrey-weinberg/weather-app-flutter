8095efdf8c01c9854357c06b17cb79394fac607f
jeff <jeffrey.weinberg89@gmail.com>
2020-04-28 15:52:05 -0600

    first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,41 @@
+import 'package:clima/screens/location_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:awesome_loader/awesome_loader.dart';
+import '../services/weather.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+  dynamic weatherData;
+
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => LocationScreen(locationWeather: weatherData)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AwesomeLoader(
+          loaderType: AwesomeLoader.AwesomeLoader4,
+        ),
+      ),
+    );
+  }
+}
